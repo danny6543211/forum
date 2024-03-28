6da0fc811900482fd36c1cb04f71a62b8982b08a
danny6543211 <danny6543211@gmail.com>
2024-03-28 19:53:38 +0800

    add user model

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import User
 
 # Create your views here.
+class UserAPI(viewsets.GenericViewSet):
+    def create(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        if not username or not password:
+            return Response({'msg': 'Both username and password are required.'}, 
+                            status=status.HTTP_400_BAD_REQUEST)
+        User.objects.create_user(username=username, password=password)
+        return Response({'msg': 'User created successfully.'}, status=status.HTTP_201_CREATED)
+
+            
