8e6e09157515a0fcacd7d96e3473c144ae44ef09
danny6543211 <danny6543211@gmail.com>
2024-03-28 21:46:23 +0800

    add user API

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import User
 
 # Create your views here.
 class UserAPI(viewsets.GenericViewSet):
@@ -12,7 +13,9 @@ class UserAPI(viewsets.GenericViewSet):
         if not username or not password:
             return Response({'msg': 'Both username and password are required.'}, 
                             status=status.HTTP_400_BAD_REQUEST)
+        if authenticate(username=username, password=password):
+            return Response({'msg': 'Username already exists.'}, status=status.HTTP_400_BAD_REQUEST)
         User.objects.create_user(username=username, password=password)
         return Response({'msg': 'User created successfully.'}, status=status.HTTP_201_CREATED)
 
-            
+        
--- forum/settings.py
+++ forum/settings.py
@@ -125,8 +125,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'api.User'
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
