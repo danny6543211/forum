e32fb16b13ea1a59fb2f4d9d7637ad035c3cccc5
danny6543211 <danny6543211@gmail.com>
2024-03-29 16:00:36 +0800

    add user api

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 
 router = routers.DefaultRouter()
-# router.register(r'user', views.StudentViewSet, basename='user')
+router.register(r'user', views.UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)), 
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
