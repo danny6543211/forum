5c5c11f5cf5d15c7dd44e0517281438fb3a03938
danny6543211 <danny6543211@gmail.com>
2024-03-31 21:07:36 +0800

    article

--- api/models.py
+++ api/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=50)
     bio = models.TextField(blank=True, null=True)
     birthday = models.DateField(blank=True, null=True)
-    gender = models.CharField(blank=True, max_length=10, null=True)
+    gender = models.CharField(blank=True, max_length=50, null=True)
 
     def __str__(self):
         return self.nickname
--- api/serializers.py
+++ api/serializers.py
@@ -2,23 +2,13 @@ from rest_framework import serializers
 from . import models
 
 
-class UserProfileSimpleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.UserProfile
-        fields = ['user', 'nickname'] #......
-
-class UserProfileDetailSerializer(serializers.ModelSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.UserProfile
         exclude = ['user']
 
 
-# class ArticleSimpleSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = models.Article
-#         fields = '__all__'
-
-# class ArticleDetialSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = models.Article
-#         fields = '__all__'
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Article
+        fields = '__all__'
