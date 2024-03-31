27cdcdab5eb0da71e8c4e72c09cd982ed8a1ded6
danny6543211 <danny6543211@gmail.com>
2024-03-31 21:07:53 +0800

    article

--- api/permissions.py
+++ api/permissions.py
@@ -1,9 +1,19 @@
-from rest_framework import permissions
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from .models import User
 
-class UserProfileDetailPermission(BasePermission):
+
+
+class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in SAFE_METHODS:
             return True
 
         return obj.user == request.user
+    
+class ArticlePermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        # article'is user named 'author'
+        return obj.author == request.user
