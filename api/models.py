5bd2d0341eb748ae47a269649099ddedd156aa33
danny6543211 <danny6543211@gmail.com>
2024-03-31 15:06:30 +0800

    add article model

--- api/models.py
+++ api/models.py
@@ -1,8 +1,22 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Profile(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nickname = models.TextField()
-    birthday = models.DateField(blank=True, null=True)
-    gender = models.CharField(blank=True, null=True, max_length=10)
+    nickname = models.CharField(max_length=40)
+    bio = models.TextField(blank=True)
+    birthday = models.DateField(blank=True)
+    gender = models.CharField(blank=True, max_length=10)
+
+    def __str__(self):
+        return self.nickname
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
